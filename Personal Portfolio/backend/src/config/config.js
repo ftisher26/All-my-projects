f0fb0c95df07545254916e9e2170f802cdd55506
TatoCSV <padularrosathiago26@gmail.com>
2026-06-12 14:37:36 -0300

    Add files via upload

---
+++ Personal Portfolio/backend/src/config/config.js
@@ -0,0 +1,7 @@
+export const variables = {
+  PORT: process.env.PORT || 4000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  HOST: process.env.HOST || 'localhost',
+  MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://TatoCSV:20040005500testing@testinlimits.isalwuw.mongodb.net/?appName=TestinLimits',
+}
+export default variables;
